fee9bf2dbb33e5637e557748fbcccaadc552ec9d
Lucas M. Shepherd <lucas@leoblack.com>
2023-08-04 20:05:13 -0700

    Pop update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
         {/* maximum-scale 1 meta tag need to prevent ios input focus auto zooming */}
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1 maximum-scale=1"
+          content="width=device-width, initial-scale=1 maximum-scale=1" 
         />
         <link rel="shortcut icon" href="/favicon.ico" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png"/>
